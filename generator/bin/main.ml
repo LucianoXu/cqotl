af5ccf1768f5a0b3cfda245e5fdfa21a4df93279
jam-khan <jamkhan@connect.hku.hk>
2025-04-16 23:12:20 +0200

    Updated the Readme file

--- generator/bin/main.ml
+++ generator/bin/main.ml
@@ -3,7 +3,10 @@ open Cqotl_vgc.Pretty_printer
 open Cqotl_vgc.Parser_utils
 
 let example = Seq(InitQubit "q1", ApplyUnitary ("H", ["q1"]))
-
+let () = print_endline "\n--qWhile programming language--\n"
+let () = print_endline "AST -> Pretty print"
 let () = print_endline (pretty_print_stmt example)
-
+let () = print_endline "------------------------------\n"
+let () = print_endline "Parsing -> AST -> Pretty print"
 let () = print_endline (pretty_print_stmt (parse_program ("q1 := |0>; H[q1]")))
+let () = print_endline "\n"
