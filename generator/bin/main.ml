eb4327897e19d90c92e438ff5b6fe8abb83d9cd3
jam-khan <jamkhan@connect.hku.hk>
2025-04-16 19:42:37 +0200

    Example added for parsing

--- generator/bin/main.ml
+++ generator/bin/main.ml
@@ -1,6 +1,9 @@
 open Cqotl_vgc.Ast
 open Cqotl_vgc.Pretty_printer
+open Cqotl_vgc.Parser_utils
 
 let example = Seq(InitQubit "q1", ApplyUnitary ("H", ["q1"]))
 
 let () = print_endline (pretty_print_stmt example)
+
+let () = print_endline (pretty_print_stmt (parse_program ("q1 := |0>; H[q1]")))
