1ab3da91761fd2d9cbaae788e64421a002caa9bf
Yingte Xu <lucianoxu@foxmail.com>
2025-04-17 14:38:49 +0200

    modify the syntax and parser

--- generator/lib/ast.ml
+++ generator/lib/ast.ml
@@ -6,17 +6,23 @@ type measurement_outcome  = int     (* 0 or 1 *)
 type measurement_op       = string  (* e.g., "M" *)
 
 (* qWhile Grammar:
-  S ::= skip
-    |   S1 ; S2
-    |   q := |0>
-    |   q_reg := U[q_reg]
-    |   if ...
-    |   while M[q_reg] = 1 do S od
+  Seq ::= S1 S2 ... Sn    (n >= 1)
+  S ::= skip ;
+    |   q := |0> ;
+    |   U[q_reg] ;
+    |   if ... ;
+    |   while M[q_reg] = 1 do Seq end
 *)
+
+(* Single Statements *)
 type stmt =
-  | Skip                                                                  
-  | Seq of            stmt * stmt
+  | Skip
   | InitQubit of      var
   | ApplyUnitary of   unitary_op      * qreg
-  | IfMeas of         measurement_op  * var * (measurement_outcome * stmt) list
-  | WhileMeas of      measurement_op  * var * stmt
+  | IfMeas of         measurement_op  * var * (measurement_outcome * stmt_seq) list
+  | WhileMeas of      measurement_op  * var * stmt_seq
+
+(* A Statement Sequence *)
+and stmt_seq =
+  | SingleCmd of stmt
+  | (::) of stmt * stmt_seq
