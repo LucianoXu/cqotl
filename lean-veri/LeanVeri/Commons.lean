3e67011811249e7ee730ad9cd9303c73bdf1f011
jam-khan <jamkhan@connect.hku.hk>
2025-05-20 13:29:48 +0200

    Work on common dirac terms and properties added

--- lean-veri/LeanVeri/Commons.lean
+++ lean-veri/LeanVeri/Commons.lean
@@ -5,7 +5,11 @@ Authors: Iván Renison, Jam Khan
 import LeanVeri.LinearMapPropositions
 import LeanVeri.OuterProduct
 import Mathlib
-
+import Mathlib.Analysis.InnerProductSpace.Positive
+import Mathlib.Analysis.InnerProductSpace.Projection
+import Mathlib.LinearAlgebra.Trace
+import Mathlib.Analysis.InnerProductSpace.PiL2
+import Mathlib.Algebra.Module.LinearMap.Defs
 /-!
 Some vectors and linear maps that are commonly used in quantum computing.
 -/
@@ -42,40 +46,65 @@ noncomputable def ketbraP : 𝕜² →ₗ[𝕜] 𝕜² :=
 noncomputable def ketbraM : 𝕜² →ₗ[𝕜] 𝕜² :=
   outerProduct 𝕜 ketM ketM
 
+/-- Ket plus equals !₂[1/√2, 1/√2] -/
 lemma ketP_eq : ketP = !₂[1/√2, 1/√2] := by
   unfold ketP ket0 ket1
   simp [← WithLp.equiv_symm_add, ← WithLp.equiv_symm_smul]
 
+/-- Ket minus equals !₂[1/√2, -1/√2] -/
 lemma ketM_eq : ketM = !₂[1/√2, -1/√2] := by
   unfold ketM ket0 ket1
   simp only [← WithLp.equiv_symm_sub, ← WithLp.equiv_symm_smul]
   field_simp
 
+/-- ‖|0⟩‖ = 1 -/
 lemma norm_ket0 : @norm 𝕜² _ ket0 = 1 := by
   unfold norm PiLp.instNorm
   simp [ket0]
 
+/-- ‖|1⟩‖ = 1 -/
 lemma norm_ket1 : @norm 𝕜² _ ket1 = 1 := by
   unfold norm PiLp.instNorm
   simp [ket1]
 
+/-- ‖|+⟩‖ = 1 -/
 lemma norm_ketP : @norm 𝕜² _ ketP = 1 := by
   unfold norm PiLp.instNorm
   field_simp [ketP, ket0, ket1]
 
+/-- ‖|-⟩‖ = 1 -/
 lemma norm_ketM : @norm 𝕜² _ ketM = 1 := by
   unfold norm PiLp.instNorm
   field_simp [ketM, ket0, ket1]
 
+/-- (|0⟩⟨0|)† = |0⟩⟨0| -/
 lemma isSelfAdjoint_ketbra0 : @IsSelfAdjoint (𝕜² →ₗ[𝕜] 𝕜²) _ ketbra0 :=
   IsSelfAdjoint_outerProduct_self 𝕜 ket0
 
+/-- (|1⟩⟨1|)† = |1⟩⟨1| -/
 lemma isSelfAdjoint_ketbra1 : @IsSelfAdjoint (𝕜² →ₗ[𝕜] 𝕜²) _ ketbra1 :=
   IsSelfAdjoint_outerProduct_self 𝕜 ket1
 
+/-- (|+⟩⟨+|)† = |+⟩⟨+| -/
 lemma isProjection_ketbraP : @LinearMap.isProjection 𝕜 𝕜² _ _ _ _ ketbraP :=
   isProjection_outerProduct_self_of_norm_eq_one 𝕜 norm_ketP
 
+/-- (|-⟩⟨-|)† = |-⟩⟨-| -/
 lemma isProjection_ketbraM : @LinearMap.isProjection 𝕜 𝕜² _ _ _ _ ketbraM :=
   isProjection_outerProduct_self_of_norm_eq_one 𝕜 norm_ketM
 
+/-- ⟨0|0⟩ = 1 -/
+lemma inner_ket0_ket0 : @inner 𝕜 𝕜² _ ket0 ket0 = 1 :=
+  (inner_eq_one_iff_of_norm_one norm_ket0 norm_ket0).mpr rfl
+
+/-- ⟨1|1⟩ = 1 -/
+lemma inner_ket1_ket1 : @inner 𝕜 𝕜² _ ket1 ket1 = 1 :=
+  (inner_eq_one_iff_of_norm_one norm_ket1 norm_ket1).mpr rfl
+
+/-- ⟨+|+⟩ = 1 -/
+lemma inner_ketplus_ketplus : @inner 𝕜 𝕜² _ ketP ketP = 1 :=
+  (inner_eq_one_iff_of_norm_one norm_ketP norm_ketP).mpr rfl
+
+/-- ⟨-|-⟩ = 1 -/
+lemma inner_ketplus_ketMinus : @inner 𝕜 𝕜² _ ketM ketM = 1 :=
+  (inner_eq_one_iff_of_norm_one norm_ketM norm_ketM).mpr rfl
---
+++ lean-veri/LeanVeri/Properties.lean
@@ -0,0 +1,194 @@
+/-
+Copyright (c) 2025 Jam Kabeer Ali Khan and Iván Renison. All rights reserved.
+Authors: Iván Renison and Jam Kabeer Ali Khan
+-/
+
+import LeanVeri.Projection
+import LeanVeri.LinearMapPropositions
+import Mathlib.LinearAlgebra.TensorProduct.Basic
+
+variable {𝕜 l n E F : Type*} [RCLike 𝕜] [RCLike l] [RCLike n] [CommSemiring n] [CommSemiring l]
+
+variable? [HilbertSpace 𝕜 E] => [NormedAddCommGroup E] [InnerProductSpace 𝕜 E] [CompleteSpace E]
+variable? [HilbertSpace 𝕜 F] => [NormedAddCommGroup F] [InnerProductSpace 𝕜 F] [CompleteSpace F]
+
+variable [FiniteDimensional 𝕜 E]
+variable [FiniteDimensional 𝕜 F]
+
+omit [CompleteSpace E]
+omit [CompleteSpace F]
+omit [FiniteDimensional 𝕜 F]
+
+open scoped TensorProduct
+
+-- lemma A.7
+namespace BasicProperties
+
+/-
+This lemma shows the `Scalar product equal 1` property `⟨φ|φ⟩ = 1`
+-/
+omit [FiniteDimensional 𝕜 E] in
+lemma scalar_product_eq_one (_ : 𝕜) (φ : E) :
+    ‖φ‖ = 1 → inner 𝕜 φ φ = 1 :=
+    by  intro H
+        refine (inner_eq_one_iff_of_norm_one ?_ ?_).mpr rfl
+        repeat assumption
+
+/-
+This lemma shows the `Scalar product` property `⟨φ|(c•A)|φ⟩ = c * ⟨φ|A|φ⟩`.
+-/
+lemma scalar_product (c : 𝕜) (φ : E) (A : E →ₗ[𝕜] E) (_ : LinearMap.isPositiveSemiDefinite A) :
+  inner 𝕜 φ ((c • A) φ) = c * inner 𝕜 φ (A φ) := by
+    rw [@LinearMap.smul_apply, @inner_smul_right]
+
+/-
+This lemma shows the `Addition` property `⟨φ|(A₁ + A₂)|φ⟩ = ⟨φ|A₁|φ⟩ + ⟨φ|A₁|φ⟩`.
+-/
+lemma addition (φ : E) (A₁ A₂ : E →ₗ[𝕜] E) (_ : LinearMap.isPositiveSemiDefinite A₁) (_ : LinearMap.isPositiveSemiDefinite A₂) :
+  inner 𝕜 φ ((A₁ + A₂) φ) = inner 𝕜 φ (A₁ φ) + inner 𝕜 φ (A₂ φ) := by
+    rw [@LinearMap.add_apply, @inner_add_right]
+
+/-
+This lemma shows the `reflexivity` of the inner product with operators `⟨φ|(A|ψ⟩) = ⟨ψ()
+-/
+lemma inner_rfl (φ : E) (ψ : E) (A : E →ₗ[𝕜] E) (h : LinearMap.isPositiveSemiDefinite A) :
+  inner 𝕜 φ (A ψ) = inner 𝕜 (A φ) ψ := by
+  rcases h with ⟨h_adj, h_pos⟩
+  rw [LinearMap.isSelfAdjoint_iff'] at h_adj
+  rw [← h_adj, @LinearMap.adjoint_inner_right]
+  exact congrFun (congrArg (inner 𝕜) (congrFun (congrArg DFunLike.coe (id (Eq.symm h_adj))) φ)) ψ
+
+end BasicProperties
+
+-- lemma A.8
+namespace AlgebraicProperties
+
+
+/- Auxilary lemma for adjoint-/
+lemma adjointeql (T: E →ₗ[𝕜] E)
+    (hT : LinearMap.isProjection T) : T.adjoint = T := by
+    rw [← @LinearMap.isSelfAdjoint_iff']
+    rcases hT with ⟨hT_posS, hT_proj⟩
+    rcases hT_posS with ⟨hT_self, hT_inner⟩
+    repeat assumption
+
+/-
+This lemma shows the `Zero_Multiplication` property `0A = 0`.
+-/
+lemma zero_mul
+  (A : E →ₗ[𝕜] E)
+  (_ : LinearMap.isPositiveSemiDefinite A) :
+    0 • A = (0 : E →ₗ[𝕜] E) :=
+  zero_nsmul A
+
+/-
+This lemma shows the `One_Multiplication` property `1A = A`.
+-/
+lemma one_mul
+  (A : E →ₗ[𝕜] E)
+  (_ : LinearMap.isPositiveSemiDefinite A) :
+    1 • A = A :=
+  one_nsmul A
+
+/-
+This lemma shows the `Mult_Associativity` property `a(bA) = (ab)A`.
+-/
+lemma mult_assoc
+  (a b : 𝕜)
+  (A : E →ₗ[𝕜] E)
+  (_ : LinearMap.isPositiveSemiDefinite A) :
+    a • (b • A) = (a * b) • A :=
+  smul_smul a b A
+
+/-
+This lemma shows the `Zero_Add_Identity` property `0 + A = A`.
+-/
+lemma zero_add
+  (A : E →ₗ[𝕜] E)
+  (_ : LinearMap.isPositiveSemiDefinite A) :
+    (0 : E →ₗ[𝕜] E) + A = A := by
+  rw [add_eq_right]
+
+/-
+This lemma shows the `Add_Zero_Identity` property `A + 0 = A`.
+-/
+lemma add_zero
+  (A : E →ₗ[𝕜] E)
+  (_ : LinearMap.isPositiveSemiDefinite A) :
+    A + (0 : E →ₗ[𝕜] E) = A :=
+  by rw [add_eq_left]
+
+/-
+This lemma shows the `Zero_Add_Add_Zero_Eqv` property `A + 0 = 0 + A`.
+-/
+lemma zero_add_add_zero_eq
+  (A : E →ₗ[𝕜] E)
+  (_ : LinearMap.isPositiveSemiDefinite A) :
+    A + (0 : E →ₗ[𝕜] E) = (0 : E →ₗ[𝕜] E) + A := by
+  rw [add_zero, zero_add]; repeat assumption
+
+/-
+This lemma shows the `Add_Commutativity` property `A₁ + A₂ = A₂ + A₁`.
+-/
+lemma add_comm
+  (A₁ A₂ : E →ₗ[𝕜] E)
+  (_ : LinearMap.isPositiveSemiDefinite A₁)
+  (_ : LinearMap.isPositiveSemiDefinite A₂) :
+    A₁ + A₂ = A₂ + A₁ := by
+  rw [@AddCommMonoidWithOne.add_comm]
+
+/-
+This lemma shows the `Add_Right_Associativity` property `A₁ + A₂ + A₃ = A₁ + (A₂ + A₃)`.
+-/
+lemma add_right_associativity
+  (A₁ A₂ A₃ : E →ₗ[𝕜] E)
+  (_ : LinearMap.isPositiveSemiDefinite A₁)
+  (_ : LinearMap.isPositiveSemiDefinite A₂) :
+    A₁ + A₂ + A₃ = A₁ + (A₂ + A₃) := by
+  rw [@AddSemigroup.add_assoc]
+
+/-
+This lemma shows the `Add_Left_Associativity` property `A₁ + A₂ + A₃ = (A₁ + A₂) + A₃`.
+-/
+lemma add_left_associativity
+  (A₁ A₂ A₃ : E →ₗ[𝕜] E)
+  (_ : LinearMap.isPositiveSemiDefinite A₁)
+  (_ : LinearMap.isPositiveSemiDefinite A₂)
+  (_ : LinearMap.isPositiveSemiDefinite A₃) :
+    A₁ + A₂ + A₃ = (A₁ + A₂) + A₃ := by
+  (expose_names; rw [add_right_associativity A₁ A₂ A₃ h h_1])
+
+/-
+This lemma shows the `Add_Left_Right_Associativity_Eqv` property `A₁ + (A₂ + A₃) = (A₁ + A₂) + A₃`.
+-/
+lemma add_left_right_associativity_eqv
+  (A₁ A₂ A₃ : E →ₗ[𝕜] E)
+  (_ : LinearMap.isPositiveSemiDefinite A₁)
+  (_ : LinearMap.isPositiveSemiDefinite A₂)
+  (_ : LinearMap.isPositiveSemiDefinite A₃) :
+    A₁ + (A₂ + A₃) = (A₁ + A₂) + A₃ := by
+  rw [add_left_associativity, add_right_associativity]; repeat assumption
+
+/-
+This lemma shows the `Zero_Prod_Identity` property `0 ⊗ A = 0`.
+-/
+lemma zero_prod_identity (A : E →ₗ[𝕜] E) (_: LinearMap.isPositiveSemiDefinite A) :
+  TensorProduct.map (0 : F →ₗ[𝕜] F) A = 0 := by
+    rw [← TensorProduct.map_zero_left A]
+
+/-
+This lemma shows the `Prod_Zero_Identity` property `A ⊗ 0 = 0`.
+-/
+lemma prod_zero_identity (A : E →ₗ[𝕜] E) (_: LinearMap.isPositiveSemiDefinite A) :
+  TensorProduct.map A (0 : F →ₗ[𝕜] F) = 0 := by
+    rw [← TensorProduct.map_zero_right A]
+
+/-
+This lemma shows the `zero_prod_prod_zero_eqv` property `0 ⊗ A = A ⊗ 0`.
+-/
+lemma zero_prod_prod_zero_eqv (A :  E →ₗ[𝕜] E) (_: LinearMap.isPositiveSemiDefinite A) :
+  TensorProduct.map (0 : E →ₗ[𝕜] E) A  = TensorProduct.map A (0 : E →ₗ[𝕜] E) := by
+    rw [prod_zero_identity, zero_prod_identity]
+    repeat assumption
+
+end AlgebraicProperties
